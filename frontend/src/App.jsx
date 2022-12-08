0f80f84ff2683a75f9e119985fca7c6e4d1ed589
Amina HAKIMI <amina.hakimi86@gmail.com>
2022-12-08 17:29:07 +0100

    Mise en place du html et du css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import NavBar from "@components/NavBar";
 
 function App() {
   return (
-    <div className="text-red">
-      <p className="text-blue-500">coucou</p>
+    <div>
+      <NavBar />
     </div>
   );
 }
