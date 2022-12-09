5f0c39afe78ca0e1e83315cdca321763a4b94b11
Amina HAKIMI <amina.hakimi86@gmail.com>
2022-12-09 09:49:50 +0100

    Ajout des liens de la navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import NavBar from "@components/NavBar";
 
 function App() {
   return (
-    <div>
-      <NavBar />
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "@assets/logo.png";
 import menuBurger from "@assets/menuBurger.png";
 import close from "@assets/close.png";
@@ -8,26 +9,42 @@ function NavBar() {
     <div className="w-full fixed top-0 left-0">
       <div className="md:flex items-center justify-between py-4 md:px-10 px-7">
         <div className="cursor-pointer md:px-10 px-7">
-          <img
-            src={logo}
-            alt="logo"
-            width="100"
-            height="100"
-            className="mx-8"
-          />
+          <Link to="/">
+            <img
+              src={logo}
+              alt="logo"
+              width="100"
+              height="100"
+              className="mx-8"
+            />
+          </Link>
         </div>
         <div className="absolute right-8 top-6 cursor-pointer md:hidden">
           <img src={menuBurger} alt="BurgerMenu" width="40" height="30" />
           <img src={close} alt="close" width="30" height="30" />
         </div>
         <ul className="md:flex md:items-center md:pb-0 pb-12 max-sm:bg-current absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in">
-          <li className="md:ml-8 text-white text-xl md:my-0 my-5">ACCUEIL</li>
-          <li className="md:ml-8 text-white text-xl md:my-0 my-5 ">A PROPOS</li>
-          <li className="md:ml-8 text-white text-xl md:my-0 my-5">
-            COMPÉTENCES
-          </li>
-          <li className="md:ml-8 text-white text-xl md:my-0 my-5">PORTFOLIO</li>
-          <li className="md:ml-8 text-white text-xl">CONTACT</li>
+          <Link to="/">
+            <li className="md:ml-8 text-white text-xl md:my-0 my-5">ACCUEIL</li>
+          </Link>
+          <Link to="/APROPOS">
+            <li className="md:ml-8 text-white text-xl md:my-0 my-5">
+              A PROPOS
+            </li>
+          </Link>
+          <Link to="/COMPÉTENCES">
+            <li className="md:ml-8 text-white text-xl md:my-0 my-5">
+              COMPÉTENCES
+            </li>
+          </Link>
+          <Link to="/PORTFOLIO">
+            <li className="md:ml-8 text-white text-xl md:my-0 my-5">
+              PORTFOLIO
+            </li>
+          </Link>
+          <Link to="/CONTACT">
+            <li className="md:ml-8 text-white text-xl">CONTACT</li>
+          </Link>
         </ul>
       </div>
     </div>
